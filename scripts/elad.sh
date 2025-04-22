c8bdeedc8c3533ff600229141325dd56ce91f1e2
Elad Kohavi <elad.kohavi@linearb.io>
2025-04-22 18:59:15 +0300

    Checking LFS

--- scripts/elad.sh
+++ scripts/elad.sh
@@ -1,1 +1,3 @@
-execute the following file
+version https://git-lfs.github.com/spec/v1
+oid sha256:bd62b44553715b16fb6d7071b93262845953f37dfd2fe01901b5797985228a90
+size 26
--- scripts/tag-version-and-public.sh
+++ scripts/tag-version-and-public.sh
@@ -1,25 +1,3 @@
-#!/bin/bash
-
-version=$(git describe --tags --abbrev=0 || echo '0.0.0')
-echo "version: $version"
-
-# Check if the version starts with 'v' and remove it
-if [[ $version == v* ]]; then
-    version=${version:1}
-fi
-
-IFS='.' read -r -a versions <<< "$version"
-
-major=${versions[0]}
-minor=${versions[1]}
-patch=${versions[2]}
-
-# Increment the patch version
-patch=$((patch + 1))
-
-# Create the new tag
-new_tag="v${major}.${minor}.${patch}"
-echo "new tag: $new_tag"
-
-git tag $new_tag
-git push origin $new_tag
+version https://git-lfs.github.com/spec/v1
+oid sha256:74c10d15cae434d3eee10317e7a8b5ed2f7cf235744720af89030ea0e977087f
+size 495
