ad0900fb93fb8eb271118a37966deec5aad58943
Elad Kohavi <106978846+EladKohavi@users.noreply.github.com>
2025-05-25 14:44:31 +0300

    Update kishkush.js

--- kishkush.js
+++ kishkush.js
@@ -1,1 +1,12 @@
+
+function isEven(num: number) {
+  if (num % 2 == 0) {
+    return true;
+  }
+  // Missing return statement
+}
+
+console.log("merge Bug?", isEven(5));
+console.log("merge Bug?");
+console.log("merge Bug?");
 console.log("merge Bug?");
