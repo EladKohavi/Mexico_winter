6163060683d3eb28465b2a5afd4ac1d6d90df543
Elad Kohavi <106978846+EladKohavi@users.noreply.github.com>
2026-02-16 10:56:57 +0200

    Update boston.py

--- boston.py
+++ boston.py
@@ -3,6 +3,13 @@
 # Variables
 a = 10
 b = 5
+currentDate = '16-02-2026';
+
+# Addition
+print(f"Today is: {currentDate}")
+
+//The current date should be 16-02-2026
+print(f"Today is: {Date.now()}")
 
 # Addition
 sum_result = a + b
